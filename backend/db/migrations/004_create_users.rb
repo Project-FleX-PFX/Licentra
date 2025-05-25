0b1ac7f1f4e352224d5b2fe437cf270ffaf2a011
cstrerath <salzstreri@gmail.com>
2025-05-25 10:32:04 +0200

    fix(SCRUM-174): Changed is_active back to true to allow login

--- backend/db/migrations/004_create_users.rb
+++ backend/db/migrations/004_create_users.rb
@@ -8,7 +8,7 @@ Sequel.migration do
       String :email, unique: true
       String :first_name
       String :last_name
-      Boolean :is_active, null: false, default: false
+      Boolean :is_active, null: false, default: true
       Integer :failed_login_attempts, null: false, default: 0
       DateTime :locked_at
       DateTime :last_password_reset_requested_at
