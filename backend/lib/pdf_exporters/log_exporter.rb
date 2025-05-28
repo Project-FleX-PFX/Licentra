942182fda8667bb47f3abdf6438e344cb951d382
Christian <christiankittler@gmx.de>
2025-05-28 15:16:21 +0200

    chore(pdf,SCRUM-133-135): adjusted the colors and layout

--- backend/lib/pdf_exporters/log_exporter.rb
+++ backend/lib/pdf_exporters/log_exporter.rb
@@ -6,11 +6,12 @@ require 'prawn/table'
 module PdfExporters
   module LogExporter
     DEFAULT_FONT = 'Helvetica'
-    BRAND_COLOR = '2551D9'
+
+    BRAND_COLOR = '0D47A1'
     HEADER_TEXT_COLOR = 'FFFFFF'
-    BODY_TEXT_COLOR = '000000'
+    BODY_TEXT_COLOR = '333333'
     ROW_COLOR_EVEN = 'FFFFFF'
-    ROW_COLOR_ODD = 'EFEFEF'
+    ROW_COLOR_ODD = 'E3F2FD'
 
     def self.generate_assignment_logs_pdf(logs, filters_applied, current_user_info)
       pdf = Prawn::Document.new(page_size: 'A4', margin: [40, 50, 40, 50], page_layout: :landscape)
@@ -53,7 +54,7 @@ module PdfExporters
                   header: true,
                   width: pdf.bounds.width,
                   row_colors: [ROW_COLOR_EVEN, ROW_COLOR_ODD],
-                  cell_style: { size: 8, padding: [3, 5, 3, 5], border_width: 0.5 }) do
+                  cell_style: { size: 8, padding: [4, 5, 4, 5], border_width: 0.5, border_color: 'CCCCCC' }) do
           row(0).background_color = BRAND_COLOR
           row(0).text_color = HEADER_TEXT_COLOR
         end
@@ -99,7 +100,7 @@ module PdfExporters
                   header: true,
                   width: pdf.bounds.width,
                   row_colors: [ROW_COLOR_EVEN, ROW_COLOR_ODD],
-                  cell_style: { size: 8, padding: [3, 5, 3, 5], border_width: 0.5 }) do
+                  cell_style: { size: 8, padding: [4, 5, 4, 5], border_width: 0.5, border_color: 'CCCCCC' }) do
           row(0).background_color = BRAND_COLOR
           row(0).text_color = HEADER_TEXT_COLOR
         end
@@ -127,14 +128,15 @@ module PdfExporters
     def self.header(pdf, title, filters_applied_text, current_user_info)
       pdf.canvas do
         pdf.fill_color BRAND_COLOR
-        pdf.fill_rectangle [pdf.bounds.left, pdf.bounds.top + 25], pdf.bounds.width, 25
+        pdf.fill_rectangle [pdf.bounds.left, pdf.bounds.top + 20], pdf.bounds.width, 20
         pdf.fill_color HEADER_TEXT_COLOR
-        pdf.text_box title, at: [pdf.bounds.left + 10, pdf.bounds.top + 20], size: 12, style: :bold
+        pdf.text_box title, at: [pdf.bounds.left + 10, pdf.bounds.top + 16], size: 10, style: :bold, height: 18,
+                            valign: :center
       end
-      pdf.move_down 35
+      pdf.move_down 30
 
       pdf.fill_color BODY_TEXT_COLOR
-      pdf.text title, size: 18, style: :bold, align: :center
+      pdf.text title, size: 16, style: :bold, align: :center
       pdf.move_down 10
       pdf.text "Exported by: #{current_user_info}", size: 8
       pdf.text "Exported at: #{Time.now.getlocal.strftime('%Y-%m-%d %H:%M:%S %Z')}", size: 8
